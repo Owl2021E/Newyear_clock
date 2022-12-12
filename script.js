36db1105a11b173cfdb62e6401a91646622f892a
Owl2021E <94488218+Owl2021E@users.noreply.github.com>
2022-12-12 22:18:37 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const daysEl=document.getElementById('days');
+const hoursEl=document.getElementById('hours');
+const minsEl=document.getElementById('mins');
+const secondsEl=document.getElementById('seconds');
+const NewYear="1 Jan 2023";
+function countdown() {
+    const newYearDate= new Date(NewYear);
+    const currentDate= new Date();
+    const totalSeconds=Math.floor(newYearDate-currentDate)/1000;
+    const d=Math.floor(totalSeconds/3600/24);
+    const h=Math.floor(totalSeconds/3600)%24;
+    const m=Math.floor(totalSeconds/60)%60;
+    const s=Math.floor(totalSeconds)%60;
+    console.log(d,h,m,s);
+    daysEl.innerHTML=d;
+    hoursEl.innerHTML=h;
+    minsEl.innerHTML=m;
+   secondsEl.innerHTML=s;
+};
+
+//function formatTime(time) {
+ // return time<10? '0${time}':time;
+//}
+// initial call
+countdown();
+// convert mili sec to seconds
+setInterval(countdown,1000);
