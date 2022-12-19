759ebae3c4254688dabf65b65a7665b536c6806c
Evgeny Bogomolny <94488218+Owl2021E@users.noreply.github.com>
2022-12-19 16:11:10 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const secondsEl=document.getElementById('seconds');
 const NewYear="1 Jan 2023";
 function countdown() {
     const newYearDate= new Date(NewYear);
+    // build in "new date" function
     const currentDate= new Date();
     const totalSeconds=Math.floor(newYearDate-currentDate)/1000;
     const d=Math.floor(totalSeconds/3600/24);
@@ -12,15 +13,16 @@ function countdown() {
     const m=Math.floor(totalSeconds/60)%60;
     const s=Math.floor(totalSeconds)%60;
     console.log(d,h,m,s);
-    daysEl.innerHTML=d;
-    hoursEl.innerHTML=h;
-    minsEl.innerHTML=m;
-   secondsEl.innerHTML=s;
+    //changing format adding "zero" infront
+    daysEl.innerHTML=formatTime(d);
+    hoursEl.innerHTML=formatTime(h);
+    minsEl.innerHTML=formatTime(m);
+   secondsEl.innerHTML=formatTime(s);
 };
 
-//function formatTime(time) {
- // return time<10? '0${time}':time;
-//}
+function formatTime(time) {
+  return time<10 ? (`0${time}`):time;
+}
 // initial call
 countdown();
 // convert mili sec to seconds
